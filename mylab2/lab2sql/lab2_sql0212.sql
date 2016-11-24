bd02223e62b85ade003058e1194ddca44e47dee2
LydiaLinaWang <wangln0927@163.com>
2016-11-24 15:40:46 +0800

    request 5 updated

--- mylab2/lab2sql/lab2_sql0212.sql
+++ mylab2/lab2sql/lab2_sql0212.sql
@@ -15,8 +15,10 @@ and   rs.real_date = '2016-11-01';
 
 
 --REQUEST 5
-insert into temp(train_id,dep_station,dest_station,,leaving_time,seat_type)
-select rs.train_id,t1.station_name,t2.station_name,t2.hard_seat_price-t1.hard_seat_price, 'hard_seat'
+
+--hard_seat
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'hard_seat',t2.hard_seat_price-t1.hard_seat_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
 from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
 where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
 and s1.city_name = '北京'
@@ -29,10 +31,118 @@ and t1.station_name = s1.station_name
 and t2.station_name = s2.station_name
 and (t1.hard_seat_price != 0 or t1.station_order=1)
 and t2.hard_seat_price != 0
-group by rs.train_id,t1.station_name,t2.station_name,t1.hard_seat_price,t2.hard_seat_price
+group by rs.train_id,t1.station_name,t2.station_name,t2.hard_seat_price-t1.hard_seat_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
 having min(rs.hard_seat) > 0;
 
-select * from temp order by
+--soft_seat
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'soft_seat',t2.soft_seat_price-t1.soft_seat_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.soft_seat_price != 0 or t1.station_order=1)
+and t2.soft_seat_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.soft_seat_price-t1.soft_seat_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.soft_seat) > 0;
+
+--hard_berth_upper
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'hard_berth_upper',t2.hard_berth_upper_price-t1.hard_berth_upper_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.hard_berth_upper_price != 0 or t1.station_order=1)
+and t2.hard_berth_upper_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.hard_berth_upper_price-t1.hard_berth_upper_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.hard_berth_upper) > 0;
+
+--hard_berth_middle
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'hard_berth_middle',t2.hard_berth_middle_price-t1.hard_berth_middle_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.hard_berth_middle_price != 0 or t1.station_order=1)
+and t2.hard_berth_middle_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.hard_berth_middle_price-t1.hard_berth_middle_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.hard_berth_middle) > 0;
+
+--hard_berth_lower
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'hard_berth_lower',t2.hard_berth_lower_price-t1.hard_berth_lower_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.hard_berth_lower_price != 0 or t1.station_order=1)
+and t2.hard_berth_lower_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.hard_berth_lower_price-t1.hard_seat_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.hard_berth_lower) > 0;
+
+--soft_berth_upper
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'soft_berth_upper',t2.soft_berth_upper_price-t1.soft_berth_upper_lower_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.soft_berth_upper_price != 0 or t1.station_order=1)
+and t2.soft_berth_upper_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.soft_berth_upper_price-t1.soft_berth_upper_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.soft_berth_upper) > 0;
+
+--soft_berth_lower
+insert into temp(train_id,dep_station,dest_station,seat_type,one_price,real_date,dep_date,dep_station_order,dest_station_order,leaving_time,duration)
+select rs.train_id,t1.station_name,t2.station_name,'soft_berth_lower',t2.soft_berth_lower_price-t1.soft_berth_lower_price,rs.real_date,'2016-11-01',t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+from remaining_seat as rs, train as t1, train as t2, station as s1, station as s2
+where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
+and s1.city_name = '北京'
+and s2.city_name = '沈阳'
+and rs.train_id = t1.train_id
+and t1.train_id = t2.train_id
+and rs.station_order between t1.station_order + 1 and t2.station_order
+and t1.station_order < t2.station_order
+and t1.station_name = s1.station_name
+and t2.station_name = s2.station_name
+and (t1.soft_berth_lower_price != 0 or t1.station_order=1)
+and t2.soft_berth_lower_price != 0
+group by rs.train_id,t1.station_name,t2.station_name,t2.soft_berth_lower_price-t1.soft_berth_lower_price,rs.real_date,t1.station_order,t2.station_order,t1.leaving_time,t2.duration-t1.duration
+having min(rs.soft_berth_lower) > 0;
+
+select * from temp order by $1;
 
 
 select rs1.train_id, rs2.train_id,s2.station_name, 'hard_seat'
@@ -40,7 +150,7 @@ from remaining_seat as rs1, remaining_seat as rs2, train as t1, train as t2, tra
 where 
     rs1.real_date = '2016-11-01'
 and rs2.real_date = '2016-11-01'
-and s1.city_name = '沈阳'
+and s1.city_name = '北京'
 and s3.city_name = '上海'
 
 and rs1.train_id = t1.train_id
@@ -68,7 +178,7 @@ and t4.station_name = s3.station_name
 and (t3.hard_seat_price != 0 or t3.station_order=1)
 and t3.hard_seat_price != 0
 
-group by rs1.train_id, rs2.train_id,s2.station_name
+group by rs1.train_id, rs2.train_id 
 having (min(rs1.hard_seat) > 0 and min(rs2.hard_seat) > 0);
 
 
@@ -123,8 +233,8 @@ select o.order_id,o.real_date,t.leaving_time,o.dep_station, o.dest_station, o.on
 from train as t, orders as o
 where t.train_id = o.train_id
 and t.station_name = o.dep_station
-and $1 <= o.real_date+ t.leaving_days_offset
-and o.real_date+ t.leaving_days_offset<= $2
+and $1 <= o.real_date
+and o.real_date<= $2
 and o.passenger_id = $3;
 
 
@@ -144,20 +254,3 @@ group by train_id
 order by Number desc
 limit 10;
 
-update remaining_seat as rs
-set rs.hard_seat= rs.hard_seat-1;
-select rs.train_id,t1.station_name,t2.station_name,t2.hard_seat_price-t1.hard_seat_price, 'hard_seat'
-from , train as t1, train as t2, station as s1, station as s2
-where rs.real_date = date'2016-11-01'-t1.leaving_days_offset
-and s1.city_name = '北京'
-and s2.city_name = '沈阳'
-and rs.train_id = t1.train_id
-and t1.train_id = t2.train_id
-and rs.station_order between t1.station_order + 1 and t2.station_order
-and t1.station_order < t2.station_order
-and t1.station_name = s1.station_name
-and t2.station_name = s2.station_name
-and (t1.hard_seat_price != 0 or t1.station_order=1)
-and t2.hard_seat_price != 0
-group by rs.train_id,t1.station_name,t2.station_name,t1.hard_seat_price,t2.hard_seat_price
-having min(rs.hard_seat) > 0;
